52ca74fa3d3830d9d3efb03a20e48a7624907cdc
Olman Urena Herrera <olmaneuh@Olmans-MacBook-Pro.local>
2025-02-23 13:21:06 -0600

    Adding a new sample

---
+++ 001_ollama/README.md
@@ -0,0 +1,35 @@
+# Intro App With Ollama
+
+## Overview
+
+This project is a simple Todo List App generator built with [LangChain](https://www.langchain.com/) and [Ollama](https://ollama.com/) to showcase the capabilities in developing LLMs powered applications.
+
+## Running The Application
+
+1. Set up the environment:
+
+   Follow the steps detailed in [the main README.md file.](../README.md)
+
+2. Download and install [Ollama](https://ollama.com/) from https://ollama.com/download.
+
+3. Open a terminal: 
+
+   Execute the command to download and try the model.
+
+   ```sh
+   ollama run llama3.2
+   ```
+
+4. Exit and navigate to your project directory:
+
+   ```sh
+   /bye 
+
+   cd /change/for/your/project/directory
+   ```
+
+5. Start the app:
+
+   ```sh
+   python main.py
+   ```
---
+++ 001_ollama/main.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
+
+
+USER_INFO: str = (
+    "Jordan Rivers, 29, is an urban planner passionate about sustainable city design. "
+    "Outside work, they enjoy photography, local history, and digital art, "
+    "blending creativity with a vision for greener communities."
+)
+
+TODO_LIST_TEMPLATE: str = (
+    "Given the following information about a fictional user:\n{user_info}\n"
+    "Create a todo list for a regular weekday."
+)
+
+
+def create_todo_list(user_info: str) -> str:
+    """
+    LangChain with Ollama code.
+    Generates a todo list based on the provided user information.
+    """
+
+    # create a prompt template
+    prompt_template = PromptTemplate(
+        input_variables=["user_info"], template=TODO_LIST_TEMPLATE
+    )
+
+    # instance of the llm that will be used
+    llm = ChatOllama(model="llama3.2")
+
+    # define a chain to generate the todo list
+    chain = prompt_template | llm | StrOutputParser()
+
+    # return the generated todo list as a string.
+    return chain.invoke(input={"user_info": user_info})
+
+
+def main() -> None:
+    """Main function to run the application."""
+
+    # load environment variables
+    load_dotenv()
+
+    # create todo list
+    todo_list = create_todo_list(USER_INFO)
+
+    # print the todo list created
+    print(todo_list)
+
+
+if __name__ == "__main__":
+    main()
